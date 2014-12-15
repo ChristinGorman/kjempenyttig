06279fb56b3a48a73b0f058b457b92bf710426ca
ChristinGorman <christin.gorman@gmail.com>
2014-12-15 22:19:28 +0100

    OMG I wrote some javadoc

--- src/main/java/no/e/nyttig/Elvis.java
+++ src/main/java/no/e/nyttig/Elvis.java
@@ -7,6 +7,33 @@ import javassist.util.proxy.ProxyObject;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+
+/**
+ * Groovy has an ?: operator. C# has an ?: operator. I work on java projects and I want one too, God damn it!
+ * Inspired by a code base filled with
+ *
+ * if (a != null && a.getB() != null && a.getB().getC() != null) {
+ *     return a.getB().getC().getAddress();
+ * }
+ *
+ * I would like to be able to write:
+ * A?:getB()?:getC()?:getAddress();
+ *
+ * With this class you can't do anything near that nice, but at least you can now write:
+ *
+ * Elvis.nullSafe(A.class, a).getB().getC().getAddress();
+ *
+ * Actually I would do a static import (import static no.e.nyttig.Elvis.nullSafe;) so it would look like this:
+ * nullSafe(A.class, a).getB().getC().getAddress();
+ *
+ * WARNING:
+ * This only works if all classes (except the last one are non-final and have default constructors)
+ * It will also take far more time as it involves making dynamic proxies
+ * Oh well.
+ * It was fun to write at least
+ *
+ * @param <T>
+ */
 public class Elvis<T> {
     private Class<T> klass;
 
